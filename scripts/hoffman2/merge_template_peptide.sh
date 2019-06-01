065304a80d20270cf3799493097421955d6ac6cc
Eric Dang <edang830@gmail.com>
2019-06-01 15:16:23 -0700

    modify hoffman scripts

--- scripts/hoffman2/merge_template_peptide.sh
+++ scripts/hoffman2/merge_template_peptide.sh
@@ -2,13 +2,12 @@
 #$ -cwd
 #$ -o ../output/merge_temp_pep.txt
 #$ -j y
-#$ -l h_data=12000M,h_rt=15:00:00
-#$ -pe shared 4
-#$ -t 2-5:1
+#$ -l h_data=12000M,h_rt=5:00:00
+#$ -t 1-1000:1
 
 . /u/local/Modules/default/init/modules.sh
 module load python/anaconda3
 
 source activate rdkit
 
-python ./merge_template_peptide.py -pin length${SGE_TASK_ID}_all.json
+python ./merge_template_peptide.py --fp_pep /u/scratch/e/ericdang/peptides/ --f_pep length2_all_${SGE_TASK_ID}*.json --fp_out /u/scratch/e/ericdang/template_peptides/
--- scripts/hoffman2/run.sh
+++ scripts/hoffman2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-JOB1="gen_pep"
+# JOB1="gen_pep"
 
-qsub -N $JOB1 generate_peptides.sh
+# qsub -N $JOB1 generate_peptides.sh
 
-# qsub -hold_jid $JOB1 merge_template_peptide.sh
+qsub merge_template_peptide.sh
