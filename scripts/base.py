392cfcf26b81f2b3e50ea65f1f13765412555a3a
Eric Dang <edang830@gmail.com>
2019-06-01 22:28:21 -0700

    add base class

--- scripts/base.py
+++ scripts/base.py
@@ -1,15 +1,24 @@
 from pathlib import Path
 from utils import Database
+import json
 
 
 class Base():
 
-    def __init__(self, project_dir=Path(__file__).resolve().parents[1], db_rxn=False, db_mol=False):
+    def __init__(self, project_dir=Path(__file__).resolve().parents[1]):
 
         self.project_dir = project_dir
 
-        if db_rxn:
-            self.db_rxn = Database(host='localhost', port=270127, db='rxn_templates')
+    def write_json(self, data, fp_out):
 
-        if db_mol:
-            self.db_mol = Database(host='localhost', port=270127, db='molecules')
+        with open(fp_out, 'w') as f:
+            json.dump(data, f)
+
+        return True
+
+    def write_txt(self, data, fp_out):
+        with open(fp_out, 'w') as f:
+            for dp in data:
+                f.write(dp + '\n')
+
+        return True
