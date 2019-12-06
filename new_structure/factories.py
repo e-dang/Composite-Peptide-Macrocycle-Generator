1e365b7cea16412bd2fa4c8d72de9d83f18c3a29
Eric Dang <edang830@gmail.com>
2019-12-05 19:14:39 -0800

    change DataSuppliers to DataHandlers
    DataHandlers now wrap both the IO classes
    responsible for loading data as well as those
    responsible for saving the data

--- new_structure/factories.py
+++ new_structure/factories.py
@@ -7,15 +7,15 @@ import loaders
 
 class IFactory(ABC):
     """
-    Interface for classes that wrap the functionality of Transformer, DataSupplier, and IO classes together such that it
-    passes the data loaded by the DataSupplier class to the Transformer class, and passes those results to the IO class
-    so it can be saved.
+    Interface for classes that wrap the functionality of Transformer and DataHandler classes together such that it
+    passes the data loaded by the DataHandler class to the Transformer class, and passes those results back to the
+    DataHandler so it can be saved.
     """
 
     @abstractmethod
     def run(self, factory_arg):
         """
-        Abstract method for wrapping the functionality of the Transformer, DataSupplier, and IO class present in
+        Abstract method for wrapping the functionality of the Transformer and DataHandler classes present in
         factory_arg such that it is executed in a parallel fashion using the multiprossesing module.
 
         Args:
@@ -25,7 +25,7 @@ class IFactory(ABC):
     @abstractmethod
     def run_serial(self, factory_arg):
         """
-        Abstract method for wrapping the functionality of the Transformer, DataSupplier, and IO class present in the
+        Abstract method for wrapping the functionality of the Transformer and DataHandler classes present in the
         factory_arg such that it is performed in a serial fashion.
 
         Args:
@@ -40,44 +40,44 @@ class MolFactory(IFactory):
 
     def run(self, factory_arg):
 
-        mol_transformer, loader, saver = self.split_args(factory_arg)
+        mol_transformer, handler = self.split_args(factory_arg)
 
         result_data = []
         with multiprocessing.Pool() as pool:
-            for result_mols in pool.imap_unordered(mol_transformer.transform, mol_transformer.get_args(loader.load())):
+            for result_mols in pool.imap_unordered(mol_transformer.transform, mol_transformer.get_args(handler.load())):
                 for result_mol in result_mols:
                     result_data.append(result_mol)
 
-        saver.save(result_data)
+        handler.save(result_data)
 
     def run_serial(self, factory_arg):
 
-        mol_transformer, loader, saver = self.split_args(factory_arg)
+        mol_transformer, handler = self.split_args(factory_arg)
 
         result_data = []
-        for arg in mol_transformer.get_args(loader.load()):
+        for arg in mol_transformer.get_args(handler.load()):
             for result_mol in mol_transformer.transform(arg):
                 result_data.append(result_mol)
 
-        saver.save(result_data)
+        handler.save(result_data)
 
     def split_args(self, factory_arg):
         """
-        Helper method that extracts the MolTransformer, DataSupplier, and MolIO classes from the FactoryArgument class.
+        Helper method that extracts the MolTransformer and DataHandler classes from the FactoryArgument class.
 
         Args:
             factory_arg (FactoryArgument): A subclass of the FactoryArgument interface.
 
         Raises:
-            AttributeError: Raised when the provided argument doesn't contain an attribute of transformer, loader,
-            and/or saver.
+            AttributeError: Raised when the provided argument doesn't contain an attribute of 'transformer' and/or
+                'handler'.
 
         Returns:
-            tuple: The MolTransformer, DataSupplier, and MolIO class
+            tuple: The MolTransformer and DataHandler
         """
 
         try:
-            return factory_arg.transformer, factory_arg.loader, factory_arg.saver
+            return factory_arg.transformer, factory_arg.handler
         except AttributeError:
             raise AttributeError(
                 'The supplied factory_arg must have instance members \'transformer\', \'loader\', and \'saver\'')
@@ -101,16 +101,16 @@ class FilterFactory(IFactory):
 
 class IFactoryArgument(ABC):
     """
-    An interface for classes that wrap a Transformer, DataSupplier, and IO class to be used by a Factory.
+    An interface for classes that wrap Transformer and DataHandler classes together to be used by a Factory.
     """
 
     @abstractmethod
-    def __init__(self, source):
+    def __init__(self, data_format):
         """
-        An intializer method for creating the Transformer, DataSupplier, and IO classes.
+        An intializer method for creating the Transformer and DataHandler classes.
 
         Args:
-            source (str): The format of the data to load/save to.
+            data_format (str): The format of the data to load from and save to.
         """
 
 
@@ -120,8 +120,7 @@ class SideChainGenerationArgs(IFactoryArgument):
     molecules into side_chain molecules.
     """
 
-    def __init__(self, source):
+    def __init__(self, data_format):
 
         self.transformer = transformers.SideChainGenerator()
-        self.loader = loaders.SCGDataSupplier(source)
-        self.saver = mol_io.JsonSideChainIO()
+        self.handler = loaders.SCGDataHandler(data_format)
