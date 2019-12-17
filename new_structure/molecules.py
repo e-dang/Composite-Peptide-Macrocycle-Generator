9465b504011998032d185a76559971f8a7436c77
Eric Dang <edang830@gmail.com>
2019-12-17 02:47:00 -0800

    fix aldehyde carbon hydrogen count in templates

--- new_structure/molecules.py
+++ new_structure/molecules.py
@@ -122,7 +122,7 @@ class CinnamoylTemplate2(ITemplateMol):
 
     @property
     def pictet_spangler_kekule(self):
-        return f'[O:{self.PS_OXYGEN_ALDEHYDE_MAP_NUM}]=[CH1:{self.PS_CARBON_ALDEHYDE_MAP_NUM}]CC(C[*:{self.REACTION_WILDCARD1_MAP_NUM}])[C:{self.OLIGOMERIZATION_MAP_NUM}]=O'
+        return f'[O:{self.PS_OXYGEN_ALDEHYDE_MAP_NUM}]=[CH1:{self.PS_CARBON_ALDEHYDE_MAP_NUM}]CC(C[*:{self.REACTION_WILDCARD1_MAP_NUM}])[CH1:{self.OLIGOMERIZATION_MAP_NUM}]=O'
 
     @property
     def oligomerization_kekule(self):
@@ -144,7 +144,7 @@ class CinnamoylTemplate3(ITemplateMol):
 
     @property
     def pictet_spangler_kekule(self):
-        return f'[*:{self.REACTION_WILDCARD1_MAP_NUM}]CCC(C[*:{self.REACTION_WILDCARD2_MAP_NUM}])(C[CH1:{self.OLIGOMERIZATION_MAP_NUM}]=O)[C:{self.PS_CARBON_ALDEHYDE_MAP_NUM}]=[O:{self.PS_OXYGEN_ALDEHYDE_MAP_NUM}]'
+        return f'[*:{self.REACTION_WILDCARD1_MAP_NUM}]CCC(C[*:{self.REACTION_WILDCARD2_MAP_NUM}])(C[CH1:{self.OLIGOMERIZATION_MAP_NUM}]=O)[CH1:{self.PS_CARBON_ALDEHYDE_MAP_NUM}]=[O:{self.PS_OXYGEN_ALDEHYDE_MAP_NUM}]'
 
     @property
     def template_pictet_spangler_kekule(self):
