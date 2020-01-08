791d639c70879d5d5683e06dc347202a8b0acdce
Eric Dang <edang830@gmail.com>
2020-01-07 18:24:33 -0800

    add checkpointing and kwargs to FactoryArgs

--- macrocycles/factories.py
+++ macrocycles/factories.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 import data_handlers
 import generators
 import argument_producers
+import config
 
 
 class IFactory(ABC):
@@ -43,24 +44,30 @@ class MolFactory(IFactory):
 
         generator, handler, arg_producer = self.split_args(factory_arg)
 
-        result_data = []
+        self.count = 0
+        self.result_data = []
         with multiprocessing.Pool() as pool:
             for result_mols in pool.imap_unordered(generator.generate, arg_producer(handler.load())):
                 for result_mol in result_mols:
-                    result_data.append(result_mol)
+                    self.result_data.append(result_mol)
+                    self.checkpoint(handler)
 
-        handler.save(result_data)
+        handler.save(self.result_data)
+        self.count += len(self.result_data)
 
     def run_serial(self, factory_arg):
 
         generator, handler, arg_producer = self.split_args(factory_arg)
 
-        result_data = []
+        self.count = 0
+        self.result_data = []
         for arg in arg_producer(handler.load()):
             for result_mol in generator.generate(arg):
-                result_data.append(result_mol)
+                self.result_data.append(result_mol)
+                self.checkpoint(handler)
 
-        handler.save(result_data)
+        handler.save(self.result_data)
+        self.count += len(self.result_data)
 
     def split_args(self, factory_arg):
         """
@@ -83,6 +90,20 @@ class MolFactory(IFactory):
             raise AttributeError(
                 'The supplied factory_arg must have instance members \'generator\', \'loader\', and \'saver\'')
 
+    def checkpoint(self, handler):
+        """
+        Helper method that saves the result_data once it reaches a certain capacity and resets result_data to an empty
+        list.
+
+        Args:
+            handler (IDataHandler): An implementation of IDataHandler.
+        """
+
+        if len(self.result_data) >= config.CAPACITY:
+            handler.save(self.result_data)
+            self.count += len(self.result_data)
+            self.result_data = []
+
 
 class IFactoryArgument(ABC):
     """
@@ -90,7 +111,7 @@ class IFactoryArgument(ABC):
     """
 
     @abstractmethod
-    def __init__(self):
+    def __init__(self, **kwargs):
         """
         An intializer method for creating the Generator and DataHandler classes.
         """
@@ -102,9 +123,9 @@ class SCConnectionModificationArgs(IFactoryArgument):
     molecules into sidechain molecules.
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.SCCMDataHandler()
+        self.handler = data_handlers.SCCMDataHandler(**kwargs)
         self.generator = generators.SideChainConnectionModifier(self.handler.id_iterator)
         self.arg_producer = argument_producers.NullArgProducer()
 
@@ -115,62 +136,62 @@ class MonomerGenerationArgs(IFactoryArgument):
     molecules into monomers.
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.MGDataHandler()
+        self.handler = data_handlers.MGDataHandler(**kwargs)
         self.generator = generators.MonomerGenerator(self.handler.index_iterator)
         self.arg_producer = argument_producers.NullArgProducer()
 
 
 class TestPeptideGenerationArgs(IFactoryArgument):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.TestPGDataHandler()
+        self.handler = data_handlers.TestPGDataHandler(**kwargs)
         self.generator = generators.PeptideGenerator()
         self.arg_producer = argument_producers.TestPeptideGeneratorArgProducer()
 
 
 class PublicationPeptideGenerationArgs(IFactoryArgument):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.PublicationPGDataHandler(peptide_length=3)
+        self.handler = data_handlers.PublicationPGDataHandler(**kwargs)
         self.generator = generators.PeptideGenerator()
         self.arg_producer = argument_producers.PeptideGeneratorArgProducer()
 
 
 class TemplatePeptideGenerationArgs(IFactoryArgument):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.TPGDataHandler()
+        self.handler = data_handlers.TPGDataHandler(**kwargs)
         self.generator = generators.TemplatePeptideGenerator()
         self.arg_producer = argument_producers.NullArgProducer()
 
 
 class MacrocycleGenerationArgs(IFactoryArgument):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.MCGDataHandler()
+        self.handler = data_handlers.MCGDataHandler(**kwargs)
         self.generator = generators.MacrocycleGenerator()
         self.arg_producer = argument_producers.MacrocycleGeneratorArgProducer()
 
 
 class UniMolecularReactionGenerationArgs(IFactoryArgument):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.UMRGDataHandler()
+        self.handler = data_handlers.UMRGDataHandler(**kwargs)
         self.generator = generators.UniMolecularReactionGenerator()
         self.arg_producer = argument_producers.CartesianProductArgProducer()
 
 
 class BiMolecularReactionGenerationArgs(IFactoryArgument):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
 
-        self.handler = data_handlers.BMRGDataHandler()
+        self.handler = data_handlers.BMRGDataHandler(**kwargs)
         self.generator = generators.BiMolecularReactionGenerator()
         self.arg_producer = argument_producers.CartesianProductArgProducer()
