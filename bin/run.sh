a74d0062ffc1b82bb44c0c8ff99a7eb88fc12949
Eric Dang <edang830@gmail.com>
2020-01-07 19:16:54 -0800

    fix double dash bug

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 qsub -N part1 run_part1.sh
-qsub -N part2 --hold_jid part1 run_part2.sh
-qsub -N part3_3 --hold_jid part2 run_part3_3.sh
-qsub -N part3_4 --hold_jid part2 run_part3_4.sh
-qsub -N part3_5 --hold_jid part2 run_part3_5.sh
+qsub -N part2 -hold_jid part1 run_part2.sh
+qsub -N part3_3 -hold_jid part2 run_part3_3.sh
+qsub -N part3_4 -hold_jid part2 run_part3_4.sh
+qsub -N part3_5 -hold_jid part2 run_part3_5.sh
