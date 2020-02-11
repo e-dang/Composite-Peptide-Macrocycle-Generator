e72369471777efa34bd313512682c8951fc25ec3
Eric Dang <edang830@gmail.com>
2020-02-11 14:08:55 -0800

    Fix which script to execute

--- bin/run_part4_3.sh
+++ bin/run_part4_3.sh
@@ -11,4 +11,4 @@ module load python/anaconda3
 
 source activate rdkit
 
-python ../macrocycles/run_part3.py --peptide_len 3 --num_jobs 1500 --num $SGE_TASK_ID --num_macrocycles 333333 > ../output/part4_3_${SGE_TASK_ID}.txt
+python ../macrocycles/run_part4.py --peptide_len 3 --num_jobs 1500 --num $SGE_TASK_ID --num_macrocycles 333333 > ../output/part4_3_${SGE_TASK_ID}.txt
