361dcbb8b1503fccf0996b7fbeafacff658739d7
Eric Dang <edang830@gmail.com>
2020-03-02 11:31:28 -0800

    Add sleep statements incase fs is slow

--- macrocycles/run_part1.py
+++ macrocycles/run_part1.py
@@ -1,8 +1,11 @@
 import macrocycles.runners as runners
+from time import sleep
 
 runners.reset()
 
 runners.run_sidechains()
 runners.run_monomers()
+sleep(1)
 runners.run_unimolecular_reactions()
+sleep(1)
 runners.run_bimolecular_reactions()
