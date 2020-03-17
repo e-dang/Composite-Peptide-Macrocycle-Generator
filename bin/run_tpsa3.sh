eaa6c537eb44704f57740239566cf9f3c3e86f9c
Eric Dang <edang830@gmail.com>
2020-03-16 22:23:38 -0700

    Write job scripts for batched descriptor calcs

---
+++ bin/run_tpsa3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -cwd
+#$ -o ../output/tpsa.txt
+#$ -j y
+#$ -l h_data=1000M,h_rt=02:00:00,h_vmem=8000M
+#$ -pe shared 8
+#$ -t 1-1000:1
+
+. /u/local/Modules/default/init/modules.sh
+module load python/anaconda3
+
+source activate rdkit
+
+python ../macrocycles/run_descriptors.py --tpsa --peptide_len 3 --num_jobs 1000 --num ${SGE_TASK_ID} > ../output/tpsa3_${SGE_TASK_ID}.txt
