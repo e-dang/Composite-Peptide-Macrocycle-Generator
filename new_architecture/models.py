b967c1d2ff76d7f63e3aebb584c4aa3480abaa72
Eric Dang <edang830@gmail.com>
2020-04-24 19:40:10 -0700

    Implement Monomer model

--- new_architecture/models.py
+++ new_architecture/models.py
@@ -1,13 +1,28 @@
 from rdkit import Chem
+from rdkit.Chem import AllChem
+
+PROLINE_N_TERM = Chem.MolFromSmarts('[NH;R]')
 
 
-class Sidechain:
-    TYPE = 'sidechain'
-
-    def __init__(self, binary, kekule, connection, shared_id, _id=None):
+class AbstractMolecule:
+    def __init__(self, binary, kekule, _id=None):
         self._id = _id
         self.binary = binary
         self.kekule = kekule
+
+    @property
+    def mol(self):
+        return Chem.Mol(self.binary)
+
+    def to_dict(self):
+        data = self.__dict__
+        data.pop('_id')
+        return data
+
+
+class Sidechain(AbstractMolecule):
+    def __init__(self, binary, kekule, connection, shared_id, _id=None):
+        super().__init__(binary, kekule, _id)
         self.connection = connection
         self.shared_id = shared_id
 
@@ -20,13 +35,29 @@ class Sidechain:
     def from_dict(cls, data, _id=None):
         return Sidechain(data['binary'], data['kekule'], data['connection'], data['shared_id'], _id=_id)
 
-    @property
-    def mol(self):
-        return Chem.Mol(self.binary)
 
-    def to_dict(self):
+class Monomer(AbstractMolecule):
+    PROLINE_N_TERM = Chem.MolFromSmarts('[NH;R]')
 
-        data = self.__dict__
-        data.pop('_id')
+    def __init__(self, binary, kekule, required, backbone, sidechain, connection, is_proline, imported, _id=None, index=None):
+        super().__init__(binary, kekule, _id)
+        self.index = index
+        self.required = required
+        self.backbone = backbone
+        self.sidechain = sidechain
+        self.connection = connection
+        self.is_proline = is_proline
+        self.imported = imported
 
-        return data
+    @classmethod
+    def from_mol(cls, mol, backbone, sidechain):
+        Chem.Kekulize(mol)
+        required = bool(AllChem.CalcNumAromaticRings(mol))
+        is_proline = AllChem.CalcNumAliphaticRings(mol) and mol.HasSubstructMatch(PROLINE_N_TERM)
+        return Monomer(mol.ToBinary(), Chem.MolToSmiles(mol, kekuleSmiles=True), required, backbone,
+                       sidechain.shared_id, sidechain.connection, is_proline, False)
+
+    @classmethod
+    def from_dict(cls, data, _id=None):
+        return Monomer(data['binary'], data['kekule'], data['required'], data['backbone'], data['sidechain'],
+                       data['connection'], data['is_proline'], data['imported'], _id=_id, index=data['index'])
--- tests/new_architecture/test_models.py
+++ tests/new_architecture/test_models.py
@@ -3,9 +3,6 @@ import new_architecture.models as models
 from rdkit import Chem
 from tests.new_architecture.data.mols import *
 
-SIDECHAIN_DICT = {'binary': Chem.MolFromSmiles('CC1=CC(=O)C2=C([NH]1)SC=C2').ToBinary(
-), 'kekule': 'CC1=CC(=O)C2=C([NH]1)SC=C2', 'connection': 'methyl', 'shared_id': 'q'}
-
 
 @pytest.fixture()
 def sidechain_from_mol():
@@ -15,6 +12,16 @@ def sidechain_from_mol():
 @pytest.fixture()
 def sidechain_from_dict():
     return models.Sidechain.from_dict(TEST_SIDECHAIN_3, _id='12498dfhjwu')
+
+
+@pytest.fixture()
+def monomer_from_mol():
+    return models.Monomer.from_mol(Chem.MolFromSmiles('NCC(CC1=CC=CC2=N[NH]C(=O)N12)C(=O)O'), 'beta3', models.Sidechain.from_mol(Chem.MolFromSmiles('CC1=CC=CC2=N[NH]C(=O)N12'), 'methyl', 'af'))
+
+
+@pytest.fixture()
+def monomer_from_dict():
+    return models.Monomer.from_dict(TEST_MONOMER_1, _id='qr398fhiusd')
 
 
 def test_sidechain_from_mol(sidechain_from_mol):
@@ -35,3 +42,29 @@ def test_sidechain_from_dict(sidechain_from_dict):
 
 def test_sidechain_to_dict(sidechain_from_dict):
     assert(sidechain_from_dict.to_dict() == TEST_SIDECHAIN_3)
+
+
+def test_monomer_from_mol(monomer_from_mol):
+    assert(monomer_from_mol._id == None)
+    assert(monomer_from_mol.binary != None)
+    assert(monomer_from_mol.kekule == 'NCC(CC1=CC=CC2=N[NH]C(=O)N12)C(=O)O')
+    assert(monomer_from_mol.backbone == 'beta3')
+    assert(monomer_from_mol.sidechain == 'af')
+    assert(monomer_from_mol.connection == 'methyl')
+    assert(monomer_from_mol.is_proline == False)
+    assert(monomer_from_mol.imported == False)
+
+
+def test_monomer_from_dict(monomer_from_dict):
+    assert(monomer_from_dict._id == 'qr398fhiusd')
+    assert(monomer_from_dict.binary != None)
+    assert(monomer_from_dict.kekule == 'O=C(O)[C@@H]1C[C@H](OC2=CC=CC=C2)CN1')
+    assert(monomer_from_dict.backbone == 'alpha')
+    assert(monomer_from_dict.sidechain == None)
+    assert(monomer_from_dict.connection == None)
+    assert(monomer_from_dict.is_proline == True)
+    assert(monomer_from_dict.imported == True)
+
+
+def test_monomer_to_dict(monomer_from_dict):
+    assert(monomer_from_dict.to_dict() == TEST_MONOMER_1)
