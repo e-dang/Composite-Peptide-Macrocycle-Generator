4df5f38996bd35be5fa62207561e56382487a364
Eric Dang <edang830@gmail.com>
2020-05-11 18:57:21 -0700

    Simplify parallelizer functionality

--- cpmg/parallelizers.py
+++ cpmg/parallelizers.py
@@ -3,59 +3,28 @@ import cpmg.config as config
 import cpmg.utils as utils
 
 
-class Parallelizer:
-    def __init__(self):
-        self.results = []
-
-    def save_result(self, result, data_handler):
-        self.results.append(result)
-
-        if len(self.results) == config.CAPACITY:
-            self._flush(data_handler)
-
-    def execute(self, generator, data_handler, keys):
-        self._execute(generator, data_handler, keys)
-        self._flush(data_handler)
-
-    def _execute(self, generator, data_handler, keys):
-        pass
-
-    def _flush(self, data_handler):
-        data_handler.save(self.results)
-        self.results = []
-
-
-class SingleProcess(Parallelizer):
+class SingleProcess:
     STRING = 'single'
 
-    def _execute(self, generator, data_handler, keys):
+    def run(self, generator, data):
 
-        for args in data_handler.load(*keys):
-            for result in generator.generate(args):
-                self.save_result(result, data_handler)
+        for args in data:
+            for result in generator.generate(*args):
+                yield result
 
 
-class Multiprocess(Parallelizer):
+class Multiprocess:
     STRING = 'multi'
 
-    def _execute(self, generator, data_handler, keys):
+    def run(self, generator, data):
 
         with multiprocessing.Pool(processes=config.NUM_PROCS - 1, maxtasksperchild=config.TASKS_PER_CHILD) as pool:
             # try using callback function in data_handler.load() to calcualte the number of documents being loaded in order to calculate chunksize
-            for result in pool.imap_unordered(generator.generate, data_handler.load(*keys)):
-                self.save_result(result, data_handler)
+            for result in pool.imap_unordered(generator.generate, data):
+                for record in result:
+                    yield record
 
 
 get_all_parallelizer_strings = utils.get_module_strings(__name__)
 
 create_parallelizer_from_string = utils.create_factory_function_closure(__name__, 'parallelizer')
-# def parallelizer_from_string(string):
-#     parallelizer_map = {
-#         SingleProcess.STRING: SingleProcess(),
-#         Multiprocess.STRING: Multiprocess()
-#     }
-
-#     try:
-#         return parallelizer_map[string]
-#     except KeyError:
-#         raise KeyError('Unrecognized parallelizer string!')
