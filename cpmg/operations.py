18c190b23dac8d5ed8fb5ba5eff5b3fc5da1fe69
Eric Dang <edang830@gmail.com>
2020-06-11 17:50:20 -0700

    Add imports

--- cpmg/operations.py
+++ cpmg/operations.py
@@ -51,6 +51,9 @@ def get_calculation_from_string(string):
 
 def pca_convex_hull(filepath, output):
     from scipy.spatial import ConvexHull
+    import numpy as np
+    import csv
+
     set_to_array = lambda set_data: np.array([np.array(list(point)) for point in set_data])
 
     points = {}
