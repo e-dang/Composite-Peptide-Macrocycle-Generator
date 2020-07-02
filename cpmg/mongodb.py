647e640b1d7c902377f5ca2de476a75a5499d2ed
Eric Dang <edang830@gmail.com>
2020-07-01 21:36:27 -0700

    Add validation and indices to mongo impl

--- cpmg/mongodb.py
+++ cpmg/mongodb.py
@@ -6,12 +6,43 @@ from mpi4py import MPI  # noqa
 import atexit
 from subprocess import Popen
 
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
+from pymongo import MongoClient, ASCENDING
+from pymongo.errors import ConnectionFailure, BulkWriteError
 
 import cpmg.utils as utils
 import cpmg.ranges as ranges
 import cpmg.config as config
+
+
+MOL_VALIDATOR = {
+    '$jsonSchema': {
+        'bsonType': 'object',
+        'required': ['kekule'],
+        'properties': {
+            'kekule': {
+                'bsonType': 'string',
+                'description': 'The atom kekule SMILES string of the molecule'
+            }
+        }
+    }
+}
+
+PREDICTION_VALIDATOR = {
+    '$jsonSchema': {
+        'bsonType': 'object',
+        'required': ['predictions', 'reacting_mol'],
+        'properties': {
+            'predictions': {
+                'bsonType': ['array', 'object'],
+                'description': 'The predictions'
+            },
+            'reacting_mol': {
+                'bsonType': 'string',
+                'description': 'The SMILES string of the reacting mol it is applicable to'
+            }
+        }
+    }
+}
 
 
 class DataBaseDaemon:
@@ -30,15 +61,17 @@ class DataBaseDaemon:
         cls.__DAEMON.terminate()
 
 
+# pylint: disable=no-member
 class MongoDataBase:
 
     def __init__(self):
         self.__client = None
         self.__database = None
         self.__collection = None
+        self.failed_instances = []
 
     def __repr__(self):
-        return self.COLLECTION + ' - ' + str(self.get_num_records())  # pylint: disable=no-member
+        return self.COLLECTION + ' - ' + str(self.get_num_records())
 
     def load(self, key):
         self.__make_connection()
@@ -49,7 +82,11 @@ class MongoDataBase:
 
     def save(self, data):
         self.__make_connection()
-        return self.__collection.insert_many(utils.to_list(data), ordered=False).inserted_ids
+        try:
+            return self.__collection.insert_many(utils.to_list(data), ordered=False).inserted_ids
+        except BulkWriteError as err:
+            self.failed_instances.extend([failed_instance['op'] for failed_instance in err.details['writeErrors']])
+            print(f'Failed to write {len(self.failed_instances)} {self.COLLECTION}s!')
 
     def get_num_records(self):
         self.__make_connection()
@@ -66,12 +103,17 @@ class MongoDataBase:
     def deactivate_records(self, key):
         return self.mark_complete(key)
 
+    def activate_records(self, key):
+        self.__make_connection()
+        return self.__collection.update_many({'_id': {'$in': utils.to_list(key)}}, {'$set': {'completed': False}})
+
     def __make_connection(self):
         if None in (self.__client, self.__database, self.__collection):
             DataBaseDaemon.start()
             self.__connect_to_client()
             self.__database = self.__client[config.MONGO_DB_DATABASE]
-            self.__collection = self.__database[self.COLLECTION]  # pylint: disable=no-member
+            self.__setup_validator()
+            self.__collection = self.__database[self.COLLECTION]
 
     def __connect_to_client(self):
         from time import sleep
@@ -81,6 +123,17 @@ class MongoDataBase:
                 break
             except ConnectionFailure:
                 sleep(1)
+
+    def __setup_validator(self):
+        if self.COLLECTION not in self.__database.collection_names():
+            collection = self.__database.create_collection(self.COLLECTION)
+            self.__database.command({'collMod': self.COLLECTION,
+                                     'validator': self.VALIDATOR,
+                                     'validationLevel': self.VALIDATION_LEVEL})
+            try:
+                collection.create_index(self.INDEX, unique=True)
+            except AttributeError:
+                pass
 
     def __load_no_pep_length(self, key):
         if isinstance(key.key, ranges.IndexKey):
@@ -97,54 +150,130 @@ class MongoDataBase:
 
 class ConnectionMongoRepository(MongoDataBase):
     COLLECTION = 'connection'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class BackboneMongoRepository(MongoDataBase):
     COLLECTION = 'backbone'
+    VALIDATION_LEVEL = 'strict'
+    INDEX = [('mapped_kekule', ASCENDING)]
+    VALIDATOR = {
+        '$jsonSchema': {
+            'bsonType': 'object',
+            'required': ['mapped_kekule'],
+            'properties': {
+                'kekule': {
+                    'bsonType': 'string',
+                    'description': 'The atom kekule SMILES string of the molecule'
+                }
+            }
+        }
+    }
 
 
 class TemplateMongoRepository(MongoDataBase):
     COLLECTION = 'template'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class SidechainMongoRepository(MongoDataBase):
     COLLECTION = 'sidechain'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class MonomerMongoRepository(MongoDataBase):
     COLLECTION = 'monomer'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class PeptideMongoRepository(MongoDataBase):
     COLLECTION = 'peptide'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class TemplatePeptideMongoRepository(MongoDataBase):
     COLLECTION = 'template_peptide'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class MacrocycleMongoRepository(MongoDataBase):
     COLLECTION = 'macrocycle'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class ConformerMongoRepository(MongoDataBase):
     COLLECTION = 'conformer'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = MOL_VALIDATOR
+    INDEX = [('kekule', ASCENDING)]
 
 
 class ReactionMongoRepository(MongoDataBase):
     COLLECTION = 'reaction'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = REACTION_VALIDATOR = {
+        '$jsonSchema': {
+            'bsonType': 'object',
+            'required': ['type', 'smarts'],
+            'properties': {
+                'type': {
+                    'bsonType': 'string',
+                    'description': 'The type of reaction it is'
+                },
+                'smarts': {
+                    'bsonType': 'string',
+                    'description': 'The SMARTS string that represents the reaction'
+                }
+            }
+        }
+    }
 
 
 class RegioSQMMongoRepository(MongoDataBase):
     COLLECTION = 'regiosqm'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = PREDICTION_VALIDATOR
 
 
 class pKaMongoRepository(MongoDataBase):
     COLLECTION = 'pka'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = PREDICTION_VALIDATOR
 
 
 class PeptidePlanMongoRepository(MongoDataBase):
     COLLECTION = 'peptide_plan'
+    VALIDATION_LEVEL = 'strict'
+    VALIDATOR = {
+        '$jsonSchema': {
+            'bsonType': 'object',
+            'required': ['combination', 'length'],
+            'properties': {
+                'combination': {
+                    'bsonType': ['array'],
+                    'description': 'The indices of the monomers to form into a peptide'
+                },
+                'length': {
+                    'bsonType': 'int',
+                    'description': 'The length of the resulting peptide. Note this is not always equal to the length of the combinations field'
+                }
+            }
+        }
+    }
 
 
 class MongoRepository:
